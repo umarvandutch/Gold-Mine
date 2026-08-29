8c7ad116b30d912270a40e86d747d80fc9836ffc
umarvandutch <96899121+umarvandutch@users.noreply.github.com>
2026-08-29 19:40:16 +0100

    Restart Gold Mine alerts after reboot or app update

---
+++ android-shell/app/src/main/java/com/goldmine/shell/BootReceiver.java
@@ -0,0 +1,9 @@
+package com.goldmine.shell;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+  @Override public void onReceive(Context context,Intent intent){BackgroundAlertManager.start(context);}
+}
